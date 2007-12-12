649d33cc269afb29dcae7aec8734b7556b9643ab
kesara.rathnayake <kesara.rathnayake@bb956e9d-b440-0410-9219-25bd47c5fc65>
2007-12-12 12:19:46 +0000

    Added a folder remotely

---
+++ Calculator.NET/Calculator.NET/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Calculator.NET")]
+[assembly: AssemblyDescription("Simple Calculator")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kesara Nanayakkara Rathnayake")]
+[assembly: AssemblyProduct("Calculator.NET")]
+[assembly: AssemblyCopyright("Copyright ©  2007 Kesara Nanayakkara Rathnayake")]
+[assembly: AssemblyTrademark("Calculator.NET")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("40cf2d5a-1510-491f-9f2a-ffca12240434")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
